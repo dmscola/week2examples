eaef52feda4f682afd4ae3dfed23747dcce952aa
mwnewman <mwnewman@umich.edu>
2018-09-13 10:14:01 -0400

    second commit?

--- promise.js
+++ promise.js
@@ -1,20 +1,46 @@
 
-const request = require('request') ;
 
-url = 'https://github.com/dwyl/english-words/raw/master/words.txt';
-
-let start = Date.now();
-
-request.get(url, (error, response, body)  => {
-    console.log(body);
-    let elapsed = Date.now() - start;
-    console.log('That took ' + elapsed + ' ms');
- });
-
-target = 1000000000;
-start = Date.now();
-for (let i = 0; i < target; i++) {
-    // do nothing
+function flipCoin() {
+    return (Math.random() > 0.5);
 }
-elapsed = Date.now() - start;
-console.log('It took ' + elapsed + ' ms to count to ' + target);
+
+function heads() {
+    console.log("Heads!");
+}
+
+function tails() {
+    console.log("Tails!");
+}
+
+// This is the function that the Promise will invoke when it's "go" time.
+// The parameters are *functions* that will be called depending on what happens
+function resolvePromise(resolve, reject) { 
+    if (flipCoin()) resolve();
+    else reject();
+}   
+
+let p = new Promise(resolvePromise); // this sets the Promise up, but nothing happens yet
+
+for (let i = 0; i < 10; i++) {
+    p.then(heads, tails);
+    p = new Promise(resolvePromise); // what happens if you remove this?
+}
+
+
+
+//////////////////////////////////////////
+// More often, you'll see it written like this...
+
+
+// for (let i = 0; i < 10; i++) {
+//     new Promise((resolve, reject) => {
+//         res = Math.random() > 0.5;
+//         if (res) resolve();
+//         else reject();
+//     }).then(
+//         () => {console.log("Heads!")}, 
+//         () => {console.log("Tails!")}
+//     );
+// }
+
+
